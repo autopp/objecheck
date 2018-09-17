7a0c077509b44bf12c72b3f4628121f15ffba058
autopp <autopp.inc@gmail.com>
2018-09-17 20:14:08 +0900

    feat: impl checking type

--- lib/objecheck/validator.rb
+++ lib/objecheck/validator.rb
@@ -1,2 +1,11 @@
 class Objecheck::Validator
+  def initialize(type:)
+    @type = type
+  end
+
+  def validate(target)
+    errors = []
+    errors << "shoud be a #{@type}" if !target.is_a?(@type)
+    errors
+  end
 end
--- spec/objecheck/validator_spec.rb
+++ spec/objecheck/validator_spec.rb
@@ -1,2 +1,23 @@
 describe Objecheck::Validator do
+  let(:validator) { described_class.new(**schema) }
+
+  describe '#validate' do
+    subject { validator.validate(target) }
+
+    context 'when schema require that type is a Hash' do
+      let(:schema) { { type: Hash } }
+
+      context 'and when target is a Hash' do
+        let(:target) { {} }
+
+        it { is_expected.to be_empty }
+      end
+
+      context 'and when target is a Array' do
+        let(:target) { [] }
+
+        it { is_expected.not_to be_empty }
+      end
+    end
+  end
 end
