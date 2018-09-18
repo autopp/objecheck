538ab7af1d719798efebd67fca5654836b432347
autopp <autopp.inc@gmail.com>
2018-09-18 20:25:02 +0900

    feat: impl initializer and errors of Collector

--- lib/objecheck/validator/collector.rb
+++ lib/objecheck/validator/collector.rb
@@ -1,2 +1,11 @@
+# Collector is used in Validator#validate to aggregate errors
+#
 class Objecheck::Validator::Collector
+  def initialize(validator)
+    @validator = validator
+  end
+
+  def errors
+    []
+  end
 end
--- spec/objecheck/validator/collector_spec.rb
+++ spec/objecheck/validator/collector_spec.rb
@@ -1,2 +1,14 @@
 describe Objecheck::Validator::Collector do
+  let(:collector) { described_class.new(validator) }
+  let(:validator) { ::Objecheck::Validator.new({}, {}) }
+
+  describe '#errors' do
+    subject { collector.errors }
+
+    it { is_expected.to be_a(Array) }
+
+    it 'returns different object for each call' do
+      expect(subject).not_to equal(collector.errors)
+    end
+  end
 end
