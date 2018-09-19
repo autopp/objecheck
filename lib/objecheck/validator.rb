32c3d7646eb04026caa62d73e4d7cc93fb4d35b1
autopp <autopp.inc@gmail.com>
2018-09-19 09:40:01 +0900

    feat: impl Validator#comple_schema to enable compile nested schema in a rule

--- lib/objecheck/validator.rb
+++ lib/objecheck/validator.rb
@@ -22,10 +22,9 @@ class Objecheck::Validator
     type: TypeRule
   }.freeze
 
-  def initialize(schema, rules = DEFAULT_RULES)
-    @rules = schema.map do |rule_name, param|
-      rules[rule_name].new(param)
-    end
+  def initialize(schema, rule_map = DEFAULT_RULES)
+    @rule_map = rule_map
+    @rules = compile_schema(schema)
   end
 
   def validate(target)
@@ -36,6 +35,12 @@ class Objecheck::Validator
       end
     end
     collector.errors
+  end
+
+  def compile_schema(schema)
+    schema.map do |rule_name, param|
+      @rule_map[rule_name].new(param)
+    end
   end
 end
 
