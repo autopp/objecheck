3eb83a7e2f08ad91d50aa99f8042efb63700f2b4
autopp <autopp.inc@gmail.com>
2018-09-20 19:55:59 +0900

    faet: record error with rule name

--- spec/objecheck/validator_spec.rb
+++ spec/objecheck/validator_spec.rb
@@ -16,7 +16,7 @@ describe Objecheck::Validator do
       context 'and when target is a Array' do
         let(:target) { [] }
 
-        it { is_expected.to eq(['root: the type should be a Hash (got Array)']) }
+        it { is_expected.to eq(['root: type: should be a Hash (got Array)']) }
       end
     end
   end
