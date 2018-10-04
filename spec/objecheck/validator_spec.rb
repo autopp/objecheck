085ac5b8860d68abbf0693d057d5011c96691efa
autopp <autopp.inc@gmail.com>
2018-10-04 09:44:06 +0900

    refactor: remove dependency to TypeRule from test of Validator

--- spec/objecheck/validator_spec.rb
+++ spec/objecheck/validator_spec.rb
@@ -1,33 +1,46 @@
 describe Objecheck::Validator do
-  let(:validator) { described_class.new(schema) }
+  let(:validator) { described_class.new(schema, rule_map) }
+
+  let(:rule_map) { { answer: answer_class } }
+  let(:answer_class) do
+    Class.new do
+      def initialize(_validator, param)
+        @answer = param
+      end
+
+      def validate(target, collector)
+        collector.add_error("should have answer which is #{@answer}") if target[:answer] != @answer
+      end
+    end
+  end
 
   describe '#validate' do
     subject { validator.validate(target) }
 
-    context 'when schema require that type is a Hash' do
-      let(:schema) { { type: Hash } }
+    context 'when schema requires to have answer which is 42' do
+      let(:schema) { { answer: 42 } }
 
-      context 'and when target is a Hash' do
-        let(:target) { {} }
+      context 'and when target matches schema' do
+        let(:target) { { answer: 42 } }
 
         it { is_expected.to be_empty }
       end
 
-      context 'and when target is a Array' do
-        let(:target) { [] }
+      context 'and when target dose not matches schema' do
+        let(:target) { {} }
 
-        it { is_expected.to eq(['root: type: should be a Hash (got Array)']) }
+        it { is_expected.to eq(['root: answer: should have answer which is 42']) }
       end
     end
   end
 
   describe '#compile_schema' do
-    subject { validator.compile_schema({ type: Array }) }
+    subject { validator.compile_schema({ answer: 42 }) }
 
-    let(:schema) { { type: Hash } }
+    let(:schema) { {} }
 
     it 'returns rules' do
-      expect(subject).to match({ type: a_kind_of(Objecheck::Validator::TypeRule) })
+      expect(subject).to match({ answer: a_kind_of(answer_class) })
     end
   end
 end
